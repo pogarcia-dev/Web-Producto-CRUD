b1bc49f757b7d3276702231323d3ea9ea0ad5fb3
Magh <63615725+Emgicraft@users.noreply.github.com>
2025-06-22 22:27:18 -0500

    Clase DataBase creada y configurada.

---
+++ Data/DataBase.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace ProyProductoCRUD.Data {
+    public class DataBase {
+        /// <summary>
+        /// Obtiene una conexión a la base de datos SQL Server.
+        /// </summary>
+        /// <returns>
+        /// Objecto SqlConnection configurado.
+        /// </returns>
+        public static SqlConnection ObtenerConexion() {
+            // Instanciar la clase generadora de cadenas de conexión
+            SqlConnectionStringBuilder builderCnx = new SqlConnectionStringBuilder();
+            // Configurar los parámetros de la cadena de conexión
+            builderCnx.DataSource = "localhost";    // Ubicación del servidor SQL Server
+            builderCnx.InitialCatalog = "BD_Producto";  // Nombre de la base de datos
+            builderCnx.UserID = "sa";   // Usuario de la base de datos
+            builderCnx.Password = "sql";    // Contraseña del usuario
+            // Crear la cadena de conexión
+            SqlConnection conexion = new SqlConnection(builderCnx.ConnectionString);
+            // Retornar la conexión
+            return conexion;
+        }
+    }
+}
